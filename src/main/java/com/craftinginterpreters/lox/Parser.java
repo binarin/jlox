c195d392aa8efd1aca85e7e1476811fb42d148d2
Alexey Lebedeff <binarin@binarin.info>
2025-08-06 12:54:59 +0200

    challenge: add C style comma sequencing operator

--- src/main/java/com/craftinginterpreters/lox/Parser.java
+++ src/main/java/com/craftinginterpreters/lox/Parser.java
@@ -21,7 +21,18 @@ public class Parser {
     }
 
     private Expr expression() {
-        return equality();
+        return commaSequence();
+    }
+
+    private Expr commaSequence() {
+        // equality (COMMA equality) *
+        Expr expr = equality();
+        while (match(COMMA)) {
+            Token operator = previous();
+            Expr right = equality();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+        return expr;
     }
 
     private Expr equality() {
